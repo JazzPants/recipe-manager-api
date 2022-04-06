d6fbd171ea0e5b7cb4d5e164615e2d7eaf5973f9
Leighton <leighton.l14@gmail.com>
2022-04-06 16:48:07 +1000

    7th Commit - Added dependent destroy to restaurant model, added update and destroy method in restaurant controller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -4,4 +4,49 @@ class Api::V1::RestaurantsController < ApplicationController
         render json: @restaurants
     end
 
+    def show
+        if @restaurant
+          render json: @restaurant
+        else
+          render json: @restaurant.errors
+        end
+      end
+        #new restaurant form is rendered in react app form
+    #   def new
+    #     @restaurant = Restaurant.new
+    #   end
+
+      def create
+        @restaurant = Restaurant.new(restaurant_params)
+    
+    
+        if @restaurant.save
+          render json: @restaurant
+        else
+          render json: @restaurant.errors
+        end
+      end
+
+#          edit form should be rendered in react app
+#       def edit
+#       end
+
+      def update
+        restaurant = Restaurant.find(params[:id])
+        restaurant.update(restaurant_params)
+        render json: restaurant
+      end
+
+      def destroy
+        # @restaurant.destroy
+        restaurant = Restaurant.find(params[:id])
+        restaurant&.destroy
+        render json: {notice: "Restaurant was succesfully removed."}
+      end
+
+      private
+      def restaurant_params
+        params.require(:restaurant).permit(:name, :description, :category, :location, :pricing)
+      end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
     belongs_to :user
-    has_many :ratings, through: :users
-    has_many :reviews, through: :users
+    has_many :ratings, through: :users, dependent: :destroy
+    has_many :reviews, through: :users, dependent: :destroy
 end
