3f831612364031ad924de5b15d93d580467396e5
Leighton <leighton.l14@gmail.com>
2022-04-06 20:29:22 +1000

    8th Commit - Added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,52 @@ Restaurant.create!(user_id: 1,name: "Kosuke",description: "Kosuke is a dedicated
 you can choose to devour. From rich, thick, tonkotsu black garlic,
 to more light, but still savoury shio ramen, there is a choice for
 everyone!",category: "Ramen",location: "Sydney",pricing: "average")
+
+Restaurant.create!(user_id: 1,    name: "Ramen Zundo",
+description:
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
+category: "Ramen",
+location: "Sydney",
+pricing: "low")
+Restaurant.create!(user_id: 1,    name: "RaRa Ramen",
+description:
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
+rating: 4,
+category: "Ramen",
+location: "Sydney",
+pricing: "high")
+Restaurant.create!(user_id: 1,    name: "Ippudo",
+description:
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
+rating: 4,
+category: "Ramen",
+location: "Sydney",
+pricing: "high")
+Restaurant.create!(user_id: 1,    name: "Mary's Newtown",
+description:
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
+rating: 4,
+category: "Burgers",
+location: "Sydney",
+pricing: "high")
+Restaurant.create!(user_id: 1,    name: "Charcoal Fish",
+description:
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
+rating: 5,
+category: "Burgers",
+location: "Sydney",
+pricing: "high")
+Restaurant.create!(user_id: 1,    name: "Castello's Pizza",
+description:
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
+rating: 5,
+category: "Pizza",
+location: "Sydney",
+pricing: "high")
+Restaurant.create!(user_id: 1,    name: "Macchiato",
+description:
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
+rating: 4,
+category: "Pizza",
+location: "Sydney",
+pricing: "high")
