ed9ec17ca88cd046efbb0842aa9d0b5d839d9275
Leighton <leighton.l14@gmail.com>
2022-04-10 23:38:44 +1000

    10th Commit - Added Reviews, Ratings nested routes for Restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :restaurants
+      resources :restaurants do
+        resources :ratings
+        resources :reviews
+      end
     end
   end
 end
+
+# resources :users do
+#   resources :ratings
+#   resources :reviews
+# end
