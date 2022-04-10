34de324626d52a7d4088327e4e3daefce3fd173b
Leighton <leighton.l14@gmail.com>
2022-04-10 23:33:32 +1000

    9th Commit - Added Ratings, Reviews controller files + some new User seeds

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,52 +1,56 @@
 class Api::V1::RestaurantsController < ApplicationController
-    def index
-        @restaurants = Restaurant.all
-        render json: @restaurants
+  def index
+    restaurants = Restaurant.all
+    render json: restaurants
+  end
+
+  def show
+    restaurant = Restaurant.find(params[:id])
+    if restaurant
+      render json: restaurant
+    else
+      render json: restaurant.errors
     end
+  end
 
-    def show
-        if @restaurant
-          render json: @restaurant
-        else
-          render json: @restaurant.errors
-        end
-      end
-        #new restaurant form is rendered in react app form
-    #   def new
-    #     @restaurant = Restaurant.new
-    #   end
+  #new restaurant form is rendered in react app form
+  #   def new
+  #     @restaurant = Restaurant.new
+  #   end
 
-      def create
-        @restaurant = Restaurant.new(restaurant_params)
-    
-    
-        if @restaurant.save
-          render json: @restaurant
-        else
-          render json: @restaurant.errors
-        end
-      end
+  def create
+    restaurant = Restaurant.new(restaurant_params)
 
-#          edit form should be rendered in react app
-#       def edit
-#       end
+    if restaurant.save
+      render json: restaurant
+    else
+      render json: restaurant.errors
+    end
+  end
 
-      def update
-        restaurant = Restaurant.find(params[:id])
-        restaurant.update(restaurant_params)
-        render json: restaurant
-      end
+  #          edit form should be rendered in react app
+  #       def edit
+  #       end
 
-      def destroy
-        # @restaurant.destroy
-        restaurant = Restaurant.find(params[:id])
-        restaurant&.destroy
-        render json: {notice: "Restaurant was succesfully removed."}
-      end
+  def update
+    restaurant = Restaurant.find(params[:id])
 
-      private
-      def restaurant_params
-        params.require(:restaurant).permit(:name, :description, :category, :location, :pricing)
-      end
+    restaurant.update(restaurant_params)
+    render json: restaurant
+  end
 
+  def destroy
+    # @restaurant.destroy
+    restaurant = Restaurant.find(params[:id])
+    restaurant&.destroy
+    render json: { notice: 'Restaurant was succesfully removed.' }
+  end
+
+  private
+
+  def restaurant_params
+    params
+      .require(:restaurant)
+      .permit(:name, :description, :category, :location, :pricing, :user_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,57 +6,82 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: "iloveramen33")
-Restaurant.create!(user_id: 1,name: "Kosuke",description: "Kosuke is a dedicated ramen joint with an extensive list of ramens
+User.create(name: 'iloveramen33')
+User.create(name: 'ilovepizza22')
+User.create(name: 'ilovepasta11')
+Restaurant.create!(
+  user_id: 1,
+  name: 'Kosuke',
+  description:
+    'Kosuke is a dedicated ramen joint with an extensive list of ramens
 you can choose to devour. From rich, thick, tonkotsu black garlic,
 to more light, but still savoury shio ramen, there is a choice for
-everyone!",category: "Ramen",location: "Sydney",pricing: "average")
+everyone!',
+  category: 'Ramen',
+  location: 'Sydney',
+  pricing: 'average',
+)
 
-Restaurant.create!(user_id: 1,    name: "Ramen Zundo",
-description:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
-category: "Ramen",
-location: "Sydney",
-pricing: "low")
-Restaurant.create!(user_id: 1,    name: "RaRa Ramen",
-description:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
-rating: 4,
-category: "Ramen",
-location: "Sydney",
-pricing: "high")
-Restaurant.create!(user_id: 1,    name: "Ippudo",
-description:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
-rating: 4,
-category: "Ramen",
-location: "Sydney",
-pricing: "high")
-Restaurant.create!(user_id: 1,    name: "Mary's Newtown",
-description:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
-rating: 4,
-category: "Burgers",
-location: "Sydney",
-pricing: "high")
-Restaurant.create!(user_id: 1,    name: "Charcoal Fish",
-description:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
-rating: 5,
-category: "Burgers",
-location: "Sydney",
-pricing: "high")
-Restaurant.create!(user_id: 1,    name: "Castello's Pizza",
-description:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
-rating: 5,
-category: "Pizza",
-location: "Sydney",
-pricing: "high")
-Restaurant.create!(user_id: 1,    name: "Macchiato",
-description:
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.",
-rating: 4,
-category: "Pizza",
-location: "Sydney",
-pricing: "high")
+Restaurant.create!(
+  user_id: 1,
+  name: 'Ramen Zundo',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.',
+  category: 'Ramen',
+  location: 'Sydney',
+  pricing: 'low',
+)
+Restaurant.create!(
+  user_id: 1,
+  name: 'RaRa Ramen',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.',
+  category: 'Ramen',
+  location: 'Sydney',
+  pricing: 'high',
+)
+Restaurant.create!(
+  user_id: 1,
+  name: 'Ippudo',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.',
+  category: 'Ramen',
+  location: 'Sydney',
+  pricing: 'high',
+)
+Restaurant.create!(
+  user_id: 1,
+  name: "Mary's Newtown",
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.',
+  category: 'Burgers',
+  location: 'Sydney',
+  pricing: 'high',
+)
+Restaurant.create!(
+  user_id: 1,
+  name: 'Charcoal Fish',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.',
+  category: 'Burgers',
+  location: 'Sydney',
+  pricing: 'high',
+)
+Restaurant.create!(
+  user_id: 1,
+  name: "Castello's Pizza",
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.',
+  category: 'Pizza',
+  location: 'Sydney',
+  pricing: 'high',
+)
+Restaurant.create!(
+  user_id: 1,
+  name: 'Macchiato',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent urna ex, porttitor vel libero vitae, elementum condimentum elit. Nam quis nisi fringilla, posuere purus nec, ultrices ante. Phasellus consequat sapien a erat tempor, scelerisque tempor odio ultrices. Etiam at ipsum dapibus, blandit ipsum sit amet, euismod elit. Curabitur posuere rhoncus.',
+  category: 'Pizza',
+  location: 'Sydney',
+  pricing: 'high',
+)
