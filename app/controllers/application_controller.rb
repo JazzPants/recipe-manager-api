7bb38c73169e63ef74637cdfed340cfc0d8e9a64
Leighton <leighton.l14@gmail.com>
2022-04-15 00:05:41 +1000

    19th Commit - Changed ActionController in ApplicationController from API to Base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  #TODO: need guard for "user-only dashboard, e.g. edit existing review
+  # only if logged-in to the relevant user of that review"
+  #---
   #   before_action :current_user
-
   #   def current_user
   #     @user = (User.find_by(id: session[:user_id]) || User.new)
   #   end
-
   #   def logged_in?
   #     current_user.id != nil
   #   end
-
   #   def require_logged_in
   #     return redirect_to(controller: 'sessions', action: 'new') unless logged_in?
   #   end
