eeb933c6d3eff289dec9562d106bdc8b1fa2e4a3
Leighton <leighton.l14@gmail.com>
2022-04-19 05:26:25 +1000

    32nd Commit - Updated ratings controller, updated reviews controller index, to get reviews based on user_id

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -1,2 +1,46 @@
 class Api::V1::RatingsController < ApplicationController
+  def index
+    puts "HELLO HELLO RESTAURANT ID: #{params[:restaurant_id]}"
+    if Rating.exists?(restaurant_id: params[:restaurant_id])
+      ratings = Rating.where(restaurant_id: params[:restaurant_id])
+      render json: rating
+    else
+      render json: []
+    end
+  end
+
+  def show
+    if Rating.exists?(params[:id])
+      rating = Rating.find(params[:id])
+      render json: rating
+    else
+      render json: {
+               message:
+                 "No rating exists with id: #{params[:id]} for restaurant with id: #{params[:restaurant_id]}",
+             }
+    end
+  end
+
+  def create
+    rating = Rating.new(rating_params)
+
+    if rating.save
+      render json: rating
+    else
+      render json: rating.errors
+    end
+  end
+
+  def update
+    rating = Rating.find(params[:id])
+
+    rating.update(rating_params)
+    render json: rating
+  end
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:value, :user_id, :restaurant_id)
+  end
 end
--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,20 +1,18 @@
 class Api::V1::ReviewsController < ApplicationController
   def index
     puts "HELLO HELLO RESTAURANT ID: #{params[:restaurant_id]}"
-    if Review.exists?(restaurant_id: params[:restaurant_id])
+    puts "HELLO HELLO USER ID: #{params[:user_id]}"
+    if params[:restaurant_id] &&
+         Review.exists?(restaurant_id: params[:restaurant_id])
       reviews = Review.where(restaurant_id: params[:restaurant_id])
       render json: reviews
+    elsif params[:user_id] && Review.exists?(user_id: params[:user_id])
+      reviews = Review.where(user_id: params[:user_id])
+      render json: reviews, only: %i[id content user_id restaurant_id]
     else
       render json: []
-
-      # {
-      #          message:
-      #            "No reviews exist for restaurant with id: #{params[:restaurant_id]}",
-      #        }
     end
   end
-
-  # return restaurant doesnt exist
 
   def show
     if Review.exists?(params[:id])
@@ -27,15 +25,6 @@ class Api::V1::ReviewsController < ApplicationController
              }
     end
   end
-
-  # def show
-  #   if Restaurant.exists?(params[:id])
-  #     restaurant = Restaurant.find(params[:id])
-  #     render json: restaurant
-  #   else
-  #     render json: { message: "No restaurant exists with id: #{params[:id]}" }
-  #   end
-  # end
 
   def create
     review = Review.new(review_params)
